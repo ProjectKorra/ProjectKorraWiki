e55b59912f336456917361f82933833952ae29c2
Vahagn Tovmasian <archbearvega@gmail.com>
2019-06-16 20:24:27 -0700

    Add Logo

--- source/conf.py
+++ source/conf.py
@@ -20,7 +20,7 @@
 # -- Project information -----------------------------------------------------
 
 project = 'ProjectKorra'
-copyright = '2018, ProjectKorra'
+copyright = '2019, ProjectKorra'
 author = 'ProjectKorra'
 
 # The short X.Y version
@@ -79,8 +79,8 @@ html_theme = 'sphinx_rtd_theme'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
-
+html_theme_options = {}
+html_logo = '_static/images/pklogo.png'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
